175ab748604079ade06014058af69e63f49684f2
Muthuraj D <155149096+Muthuraj-coder@users.noreply.github.com>
2026-07-01 15:03:49 +0530

    Time: 3 ms (26.05%), Space: 63.2 MB (98.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,22 +3,23 @@ class Solution {
         int [] lp = new int[nums.length];
         int [] rp = new int[nums.length];
 
-        lp[0] = 1;
-        for(int i=1;i<nums.length;i++){
-            lp[i]=lp[i-1]*nums[i-1];
-        }
+        lp[0]=1;
         rp[nums.length-1]=1;
+
+        for(int i=1;i<nums.length;i++){
+            lp[i] = lp[i-1] * nums[i-1];
+        }
+
         for(int i=nums.length-2;i>=0;i--){
-            rp[i]=rp[i+1]*nums[i+1];
+            rp[i] = rp[i+1] * nums[i+1];
         }
 
         for(int i=0;i<nums.length;i++){
-            if(i==0) nums[i]=rp[i];
+            if(i==0) nums[i] = rp[i];
             else if(i==nums.length-1) nums[i]=lp[i];
-            else nums[i]=lp[i]*rp[i];
+            else nums[i] = lp[i] * rp[i];
         }
 
         return nums;
-        
     }
 }
